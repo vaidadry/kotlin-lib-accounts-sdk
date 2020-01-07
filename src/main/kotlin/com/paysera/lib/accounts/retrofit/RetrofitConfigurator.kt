5e12f76cbe14490285f295400ac925deec14647e
Aurimas Žarskis <a.zarskis@paysera.lt>
2020-01-07 14:43:28 +0200

    Added base filter to PaymentCardDesignFilter

--- src/main/kotlin/com/paysera/lib/accounts/retrofit/RetrofitConfigurator.kt
+++ src/main/kotlin/com/paysera/lib/accounts/retrofit/RetrofitConfigurator.kt
@@ -65,7 +65,6 @@ class RetrofitConfigurator(private val accountsApiCredentials: AccountsApiCreden
         gsonBuilder.registerTypeAdapter(CardLimit::class.java, CardLimitSerializer())
         gsonBuilder.registerTypeAdapter(TransferNotification::class.java, TransferNotificationDeserializer())
         gsonBuilder.registerTypeAdapter(Date::class.java, DateSerializer())
-        gsonBuilder.registerTypeAdapter(PaymentCardDesign::class.java, PaymentCardDesignDeserializer())
 
         return GsonConverterFactory.create(gsonBuilder.create())
     }
