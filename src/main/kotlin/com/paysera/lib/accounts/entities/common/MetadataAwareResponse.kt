36f5db7e5c326d99f0e8de65e2226938df430409
Aurimas Žarskis <a.zarskis@paysera.lt>
2020-01-08 10:11:31 +0200

    Test

--- src/main/kotlin/com/paysera/lib/accounts/entities/common/MetadataAwareResponse.kt
+++ src/main/kotlin/com/paysera/lib/accounts/entities/common/MetadataAwareResponse.kt
@@ -3,7 +3,6 @@ package com.paysera.lib.accounts.entities.common
 import com.google.gson.annotations.SerializedName
 
 class MetadataAwareResponse <T> (
-    @SerializedName("items", alternate = ["card_designs"])
     val items: List<T>,
     @SerializedName("_metadata")
     val metadata: Metadata
