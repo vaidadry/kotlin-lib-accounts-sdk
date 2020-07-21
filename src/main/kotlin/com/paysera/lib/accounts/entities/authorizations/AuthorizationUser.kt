d6327e5fda5bca90ba59b2719a30536c7619477c
Lukas Šivickas <l.sivickas@gmail.com>
2020-07-21 13:02:06 +0300

    Set AuthorizationUser fields nullable

--- src/main/kotlin/com/paysera/lib/accounts/entities/authorizations/AuthorizationUser.kt
+++ src/main/kotlin/com/paysera/lib/accounts/entities/authorizations/AuthorizationUser.kt
@@ -1,6 +1,6 @@
 package com.paysera.lib.accounts.entities.authorizations
 
 data class AuthorizationUser(
-    val userId: Int,
-    val displayName: String
+    val userId: Int?,
+    val displayName: String?
 )
