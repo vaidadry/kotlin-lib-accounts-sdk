ac7ed18b5791bb78b7b0996145849f583cd05298
Vaida Dryžaitė <vaidadryzaite@Vaidas-MacBook-Pro.local>
2021-02-26 11:12:43 +0200

    removed serialized

--- src/main/kotlin/com/paysera/lib/accounts/entities/XpayToken.kt
+++ src/main/kotlin/com/paysera/lib/accounts/entities/XpayToken.kt
@@ -1,8 +1,5 @@
 package com.paysera.lib.accounts.entities
 
-import com.google.gson.annotations.SerializedName
-
 data class XpayToken(
-	@SerializedName("encrypted_data")
 	val encryptedData: String
 )
