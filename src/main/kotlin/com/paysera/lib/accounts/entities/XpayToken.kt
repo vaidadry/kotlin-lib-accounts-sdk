40e55a22425085d836989824882eafc80b6332bf
Vaida Dryžaitė <vaidadryzaite@Vaidas-MacBook-Pro.local>
2021-02-26 10:12:38 +0200

    serialized names

--- src/main/kotlin/com/paysera/lib/accounts/entities/XpayToken.kt
+++ src/main/kotlin/com/paysera/lib/accounts/entities/XpayToken.kt
@@ -1,5 +1,8 @@
 package com.paysera.lib.accounts.entities
 
+import com.google.gson.annotations.SerializedName
+
 data class XpayToken(
+	@SerializedName("encrypted_data")
 	val encryptedData: String
 )
