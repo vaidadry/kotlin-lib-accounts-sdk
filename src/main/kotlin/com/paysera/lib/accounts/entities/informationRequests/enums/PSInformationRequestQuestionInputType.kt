3f1187e2a10d8e480fa042ee8f1d758affec874e
Vaida Dryžaitė <vaidadryzaite@Vaidas-MacBook-Pro.local>
2021-04-07 11:30:19 +0300

    AML-request-fileExplanation

--- src/main/kotlin/com/paysera/lib/accounts/entities/informationRequests/enums/PSInformationRequestQuestionInputType.kt
+++ src/main/kotlin/com/paysera/lib/accounts/entities/informationRequests/enums/PSInformationRequestQuestionInputType.kt
@@ -3,5 +3,6 @@ package com.paysera.lib.accounts.entities.informationRequests.enums
 enum class PSInformationRequestQuestionInputType(val value: String) {
     INPUT_TYPE_TEXT("text"),
     INPUT_TYPE_TEXT_AREA("textarea"),
-    INPUT_TYPE_DATE("date")
+    INPUT_TYPE_DATE("date"),
+    INPUT_TYPE_FILE_EXPLANATION("fileExplanation")
 }
