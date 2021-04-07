79da35267d3b0b3f8051d5a9db3c62552b401966
Vaida Dryžaitė <vaidadryzaite@Vaidas-MacBook-Pro.local>
2021-04-07 11:18:38 +0300

    removed serialised

--- src/main/kotlin/com/paysera/lib/accounts/entities/XpayTokenRequest.kt
+++ src/main/kotlin/com/paysera/lib/accounts/entities/XpayTokenRequest.kt
@@ -1,16 +1,9 @@
 package com.paysera.lib.accounts.entities
 
-import com.google.gson.annotations.SerializedName
-
 data class XpayTokenRequest(
-	@SerializedName("app_id")
 	val appId: String,
-	@SerializedName("card_id")
 	val cardId: Int,
-	@SerializedName("device_id")
 	val deviceId: String,
-	@SerializedName("token_requestor_code")
 	val tokenRequestorCode: String,
-	@SerializedName("wallet_account_id")
 	val walletAccountId: String
 )
